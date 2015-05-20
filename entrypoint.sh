3b456edbf5b4fd30105fe4d1e43f9ade2c42f82a
SCOTT P. GALLAGHER <sgallag@gmail.com>
2015-05-20 11:03:04 -0400

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-consul agent -server -bootstrap-expect 3 -data-dir /tmp/consul -config-dir /etc/consul.d/ &
+consul agent -server -bootstrap-expect 3 -data-dir /tmp/consul -config-dir /etc/consul.d/ -ui-dir /0.5.2_web_ui&
 mysqld --wsrep-new-cluster
